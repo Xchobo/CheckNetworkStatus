1e31879a903c5e0d0198fb52407d8fe12f8061ab
Xchobo <xchobo@gmail.com>
2014-03-31 16:57:25 +0800

    Initial Commit

---
+++ CheckNetworkStatus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CheckNetworkStatus
+//
+//  Created by Xchobo on 2014/3/31.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CheckNetworkStatusTests/CheckNetworkStatusTests.m
@@ -0,0 +1,34 @@
+//
+//  CheckNetworkStatusTests.m
+//  CheckNetworkStatusTests
+//
+//  Created by Xchobo on 2014/3/31.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CheckNetworkStatusTests : XCTestCase
+
+@end
+
+@implementation CheckNetworkStatusTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
