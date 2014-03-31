374255f11ef0888183e301efa72e2c5727b06fc1
Xchobo <xchobo@gmail.com>
2014-03-31 17:48:44 +0800

    CheckNetworkStatus

--- CheckNetworkStatus/ViewController.m
+++ CheckNetworkStatus/ViewController.m
@@ -7,17 +7,46 @@
 //
 
 #import "ViewController.h"
+#import "CheckInternet.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) CheckInternet *checkinternet;
+@property (strong, nonatomic) IBOutlet UILabel *statusField;
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _checkinternet = [[CheckInternet alloc] init];
+    
+    // 0:無網路、1:行動網路、2:Wifi網路
+    int value = [_checkinternet networkCheck];
+    NSLog(@"網路狀態");
+    
+    switch (value) {
+        case 0:
+            NSLog(@"無網路");
+            [_statusField setText:@"無網路"];
+            break;
+        case 1:
+            NSLog(@"行動網路");
+            [_statusField setText:@"行動網路"];
+            break;
+        case 2:
+            NSLog(@"Wifi網路");
+            [_statusField setText:@"Wifi網路"];
+            break;
+        default:
+            break;
+    }
+
 }
 
 - (void)didReceiveMemoryWarning
